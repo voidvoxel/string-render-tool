5437ba26687078fb6f032762b37252c7453eaab2
Ashlynn Juniper <160202125+voidvoxel@users.noreply.github.com>
2025-01-03 17:03:00 -0600

    Change example output name

--- examples/hello_world.js
+++ examples/hello_world.js
@@ -2,4 +2,4 @@ const { StringRenderTool } = require("../src");
 
 const stringRenderer = new StringRenderTool({ fontSize: 12 });
 
-stringRenderer.render({ output: "hello_world.png", string: "Hello, world!" });
+stringRenderer.render({ output: "example.png", string: "Hello, world!" });
